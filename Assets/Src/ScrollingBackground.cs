d99bba9341f473330c271a12cf452218b244f7dc
mandexonla <nguyendangman0309@gmail.com>
2025-08-03 15:18:07 +0700

    feat: build scroll background

---
+++ Assets/Src/ScrollingBackground.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] private float ScrollSpeed;
+    [SerializeField] private Renderer backgroundRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundRenderer.material.mainTextureOffset += new Vector2(ScrollSpeed * Time.deltaTime, 0f);
+    }
+}
